43e7007b69701f2eaf957c1d5d9a4f47fb581dbf
PDH <pdh@PDHui-MacBookAir.local>
2022-06-22 19:26:53 +0900

    (#5) add_Movie

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('',Home.as_view()),
     path('profile/',ProfileList.as_view(),name='profile_list') ,# 프로필 리스트
     path('profile/create/',ProfileCreate.as_view(),name='profile_create') ,# 프로필 생성
-    path('watch/<str:profile_id>/',Watch.as_view,name='watch'), # watch 
+    path('watch/<str:profile_id>/',Watch.as_view(),name='watch'), # watch 
 
 
 ]
